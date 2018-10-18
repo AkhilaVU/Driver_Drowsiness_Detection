8fa18c744eef3bec702679201a0129f0cf2f8ed1
Amruthaadithyan <amruthanair2012@gmail.com>
2018-10-18 15:02:52 +0530

    haar cascade

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -5,3 +5,10 @@ import os
 print('Initialising...')
 myPath = os.getcwd()#Current Working Dir
 cascPath  = myPath + '\haarcascades\haarcascade_frontalface_default.xml'
+
+print('Setting up camera...')
+cam = cv2.VideoCapture(0)
+
+# Create the haar cascade
+print('Creating the haar cascade...')
+faceCascade = cv2.CascadeClassifier(cascPath)   # haar cascade
